1ce38037f4376d93e48cf722662f518d30ccd7b2
Hiep Le <>
2023-05-14 21:06:18 +0700

    update asset for polkadot_js wallet logo, removed unused assets

--- front/src/data/wallets.tsx
+++ front/src/data/wallets.tsx
@@ -1,23 +1,21 @@
-import IMG_MTM from '../assets/images/metamask.png';
-import IMG_TEM from '../assets/images/temple.png';
-import IMG_KUK from '../assets/images/kukai.png';
+import IMG_METAMASK from '../assets/images/metamask.png';;
+import IMG_POLKA from '../assets/images/polka_wallet.png';
 
 const wallets: {id: number, name: string, img: string, unit: string, available: boolean}[] = [
   {
     id: 1,
-    name: "Temple",
-    img: IMG_TEM,
-    unit: "XTZ",
+    name: "Metamask",
+    img: IMG_METAMASK,
+    unit: "DOT",
     available: true,
   },
   {
     id: 2,
-    name: "Kukai",
-    img: IMG_KUK,
-    unit: "XTZ",
-    available: false,
+    name: "Polkadot.JS",
+    img: IMG_POLKA,
+    unit: "DOT",
+    available: true,
   },
-  
 ]
 
 export default wallets;
